dfca3a1d8d8dba72fc888d39f355bcf2c56c6e84
segunsub <segunsubair3@gmail.com>
2020-09-24 19:42:53 +0000

    good to go

--- helloworld.js
+++ helloworld.js
@@ -1,14 +1,12 @@
 function helloWorld(){
-  console.log("Hello, World"); //I don't know why this doesn't work.
+  console.log("Hello, World"); 
 }
 
 let name = 'Ann'
 function helloName() {
- console.log (`Hello, ${name}`); //Idk why it prints out "Hello, $name" instead of "Hello, Ann"
+ console.log (`Hello, ${name}`); 
 }
 
 
-
-//Tried to invoke both functions here but I get an error :(
 helloWorld()
 helloName()
